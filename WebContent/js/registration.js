1e0dfb644be177fa6b218a23e8494844168c09e7
faishal <saiyedfaishal@gmail.com>
2012-10-16 13:38:06 +0530

    Facebook authentication function added

--- WebContent/js/registration.js
+++ WebContent/js/registration.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-	$('.btn-linkedin').click(function(){
+	// LinkedIn registration
+	$('.btn-linkedin').click(function() {
 		IN.User.authorize();
 		return false;
 	});
@@ -8,9 +9,28 @@ $(document).ready(function() {
 		IN.User.isAuthorized(function() {
 			// using js sdk get liked user details.
 			IN.API.Profile("me").fields([ "id" ]).result(function(result) {
-				// After Authentication Process to next stemp				
+				// After Authentication Process to next step
 				return;
 			});
 		});
+	});
+
+	// Facebook Registration
+	$('.btn-facebook').click(function() {
+		FB.login(function(response) {
+			if (response.authResponse) {
+				// Get User Details
+				FB.api('/me', function(respo) {
+					// After Authentication Process to next step
+				});
+
+			} else {
+				// User close auth window
+				alert('User cancelled login or did not fully authorize.');
+			}
+		}, {
+			scope : 'email'
+		});
+
 	});
 });
