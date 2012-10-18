195072b48156731acd7de8c0a95d99c8d91612b7
Kushal Pandya <kushal.pandya04@gmail.com>
2012-10-19 01:49:00 +0530

    Event handlers moved from home.html.

---
+++ WebContent/js/script.js
@@ -0,0 +1,28 @@
+var POSTBOX_BOTTOM_MARGIN = 20;
+
+$(".feed-comment-reveal").on("click", function(e){
+	e.preventDefault();
+	var commentCount = $(this).parent().find(".feed-comments li").length;
+	
+	$(this).parents().eq(2).css("marginBottom", (commentCount*10).toString()+"%");
+	$(this).hide();
+	$(".feed-comments, .feed-comment-box").slideDown();
+});
+				
+$("#postBox").on("keypress", function(e) {
+	var postBox = $(this);
+	var boxH = postBox.height();
+	if(e.keyCode == 13 && boxH <= 80)
+	{
+		postBox.height(boxH+20);
+		$(".feeds-list").css("marginTop",(POSTBOX_BOTTOM_MARGIN+130).toString()+"px");
+	}
+});
+				
+$("#postBox").on("blur", function() {
+	if($(this).val().trim() === "")
+	{
+		$(this).val("").height(20);
+		$(".feeds-list").css("marginTop","20px");
+	}
+});
