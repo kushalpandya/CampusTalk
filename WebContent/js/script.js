74bfe43f819448aea8ddb597ea86bc06bbc55e28
$tr!K3r <saiyedfaishal@gmailc.om>
2012-10-25 21:16:12 +0530

    fucntion to call Avgrund Model added (showPopup(htmlContent))

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -34,3 +34,21 @@ $(".feed-comments li").on("mouseenter", function() {
 $(".feed-comments li").on("mouseleave", function() {
 	$(this).find(".comment-action").hide();
 });
+
+//Model Windows Show Function 
+
+function showPopup(templateHTML){
+	$('#popupLink').avgrund({			
+		width: 380, // max is 640px
+		height: 280, // max is 350px
+		showClose: true, // switch to 'true' for enabling close button 
+		showCloseText: 'Close', // type your text for close button
+		closeByEscape: true, // enables closing popup by 'Esc'..
+		closeByDocument: true, // ..and by clicking document itself
+		holderClass: '', // lets you name custom class for popin holder..
+		overlayClass: '', // ..and overlay block
+		enableStackAnimation: false, // another animation type
+		onBlurContainer: '', // enables blur filter for specified block 
+		template: templateHTML
+	}).click();
+}
