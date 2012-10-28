374639efac5636a6675fc026595e6760c1a44098
faishal <saiyedfaishal@gmail.com>
2012-10-28 05:37:38 +0530

    GetParameterByName function added to read querystring

--- WebContent/js/registration.js
+++ WebContent/js/registration.js
@@ -117,3 +117,13 @@ $('#frmRegistration').submit(function(e) {
 	});
 
 })
+function getParameterByName(name) {
+	name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+	var regexS = "[\\?&]" + name + "=([^&#]*)";
+	var regex = new RegExp(regexS);
+	var results = regex.exec(window.location.search);
+	if (results == null)
+		return "";
+	else
+		return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
