958c61657090b675055ac0e877d995ec865737f8
faishal <saiyedfaishal@gmail.com>
2012-11-03 06:03:19 +0530

    db class for comment add & comment on post completed

---
+++ src/org/campustalk/sql/dbComment.java
@@ -0,0 +1,40 @@
+package org.campustalk.sql;
+
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import org.campustalk.entity.CampusTalkComment;
+import org.campustalk.entity.CampusTalkPost;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class dbComment extends DatabaseManager {
+	
+	public int createCommentOnPost(CampusTalkComment objComment){
+		int rCount = 0;
+		try {
+			this.open();
+			CallableStatement pst = CON
+					.prepareCall("{call CommentOnPost(?,?,?,?)}");
+
+			pst.setInt(1, objComment.getPostId());
+			pst.setInt(2, objComment.getUserId());
+			pst.setString(3, objComment.getDetail());
+			pst.registerOutParameter(4, Types.INTEGER);
+			pst.execute();
+			rCount = pst.getInt(4);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rCount;
+
+	}
+
+}
