af8307de4baf0c750a55fc9d928b8b98989416a3
faishal <saiyedfaishal@gmail.com>
2012-11-03 17:57:49 +0530

    remove comment reveal code and moved it to home.js

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,7 +1,7 @@
 var POSTBOX_BOTTOM_MARGIN = 20;
 var LOADER_OVERLAY = $("<div class='loading-overlay'><i class='loader-icon'></i></div>");
 
-/********************** Initializers ***********************/
+/** ******************** Initializers ********************** */
 
 $.ajaxSetup({
 	beforeSend : function(jqXHR, settings) {
@@ -12,65 +12,56 @@ $.ajaxSetup({
 	}
 });
 
-/********************** home.html script ***********************/
+/** ******************** home.html script ********************** */
+$("#postBox").on(
+		"keypress",
+		function(e) {
+			var postBox = $(this);
+			var boxH = postBox.height();
+			if (e.keyCode == 13 && boxH <= 80) {
+				postBox.height(boxH + 20);
+				$(".feeds-list").css("marginTop",
+						(POSTBOX_BOTTOM_MARGIN + 130).toString() + "px");
+			}
+		});
 
-$(".feed-comment-reveal").on("click", function(e){
-	e.preventDefault();
-	var commentCount = $(this).parent().find(".feed-comments li").length;
-	
-	$(this).parents().eq(2).css("marginBottom", (commentCount*7).toString()+"%");
-	$(this).hide();
-	$(".feed-comments, .feed-comment-box").slideDown();
-});
-				
-$("#postBox").on("keypress", function(e) {
-	var postBox = $(this);
-	var boxH = postBox.height();
-	if(e.keyCode == 13 && boxH <= 80)
-	{
-		postBox.height(boxH+20);
-		$(".feeds-list").css("marginTop",(POSTBOX_BOTTOM_MARGIN+130).toString()+"px");
-	}
-});
-				
 $("#postBox").on("blur", function() {
-	if($(this).val().trim() === "")
-	{
+	if ($(this).val().trim() === "") {
 		$(this).val("").height(20);
-		$(".feeds-list").css("marginTop","20px");
+		$(".feeds-list").css("marginTop", "20px");
 	}
 });
+function afterLoadComments() {
+	$(".feed-comments li").on("mouseenter", function() {
+		$(this).find(".comment-action").show();
+	});
 
-$(".feed-comments li").on("mouseenter", function() {
-	$(this).find(".comment-action").show();
-});
-
-$(".feed-comments li").on("mouseleave", function() {
-	$(this).find(".comment-action").hide();
-});
-
+	$(".feed-comments li").on("mouseleave", function() {
+		$(this).find(".comment-action").hide();
+	});
+}
 $(".account-tray #showMessages").on("click", function(e) {
 	e.preventDefault();
-	
+
 	$("#avgmodal").avgrund({
-		width: 300,
-		height: 200,
-		showClose: true,
-		showCloseText: '&times;',
-		holderClass: 'page-wrap',
-		showClose: true,
-		template: $("#avgmodal").html(),
-		onLoad: function() {
+		width : 300,
+		height : 200,
+		showClose : true,
+		showCloseText : '&times;',
+		holderClass : 'page-wrap',
+		showClose : true,
+		template : $("#avgmodal").html(),
+		onLoad : function() {
 			$(".account-tray").hide();
 			$("#avgmodal p").css("fontSize", "20px");
 		},
-		onUnload: function() {
+		onUnload : function() {
 			$(".account-tray").fadeIn(50);
 		}
 	}).click();
 });
 
-/********************** controlpanel.html script ***********************/
+/** ******************** controlpanel.html script ********************** */
 
 $("#ViewGroup").on("shown", function(e) {
 	var modal = $(this);
@@ -78,7 +69,7 @@ $("#ViewGroup").on("shown", function(e) {
 });
 
 $(".input-append input").on("keyup", function() {
-	if($(this).val().trim().length > 0)
+	if ($(this).val().trim().length > 0)
 		$(this).parent().find("a").removeClass("disabled");
 	else
 		$(this).parent().find("a").addClass("disabled");
@@ -86,31 +77,38 @@ $(".input-append input").on("keyup", function() {
 
 $("a[href='#CreateGroup']").on("click", function(e) {
 	e.preventDefault();
-	if(!$(this).hasClass("disabled"))
-	{
+	if (!$(this).hasClass("disabled")) {
 		var groupName = $(this).parent().find("input").val();
 		$("#CreateGroup").modal('show');
 		$("#CreateGroup").find("input[name='txtGroupName']").val(groupName);
-	}
-	else
+	} else
 		$("#CreateGroup").modal('hide');
 });
 
-$("a[href='#EditGroup']").on("click", function(e) {
-	var groupId = parseInt($(this).parents().eq(1).find("td:nth-child(1)").text());
-	var groupName = $(this).parents().eq(1).find("td:nth-child(2)").text();
-	
-	$("#EditGroup #dlgLabel").text("Edit Group - "+groupName);
-	$("#EditGroup").find("input[name='txtGroupName']").val(groupName);
-});
+$("a[href='#EditGroup']").on(
+		"click",
+		function(e) {
+			var groupId = parseInt($(this).parents().eq(1).find(
+					"td:nth-child(1)").text());
+			var groupName = $(this).parents().eq(1).find("td:nth-child(2)")
+					.text();
 
-$("a[href='#ViewGroup']").on("click", function(e) {
-	var groupId = parseInt($(this).parents().eq(1).find("td:nth-child(1)").text());
-	var groupName = $(this).parents().eq(1).find("td:nth-child(2)").text();
-	
-	$("#ViewGroup #dlgLabel").text(groupName);
-	$("#ViewGroup").css("margin-left", ($("#ViewGroup").outerWidth() / 2) * -1);
-});
+			$("#EditGroup #dlgLabel").text("Edit Group - " + groupName);
+			$("#EditGroup").find("input[name='txtGroupName']").val(groupName);
+		});
+
+$("a[href='#ViewGroup']").on(
+		"click",
+		function(e) {
+			var groupId = parseInt($(this).parents().eq(1).find(
+					"td:nth-child(1)").text());
+			var groupName = $(this).parents().eq(1).find("td:nth-child(2)")
+					.text();
+
+			$("#ViewGroup #dlgLabel").text(groupName);
+			$("#ViewGroup").css("margin-left",
+					($("#ViewGroup").outerWidth() / 2) * -1);
+		});
 
 $("a[href='#EditGroupUser']").on("click", function(e) {
 	e.preventDefault();
@@ -134,89 +132,89 @@ $("#btnCancelEdit, #btnCancelAdd").on("click", function(e) {
 
 $("a[href='#CreateUser']").on("click", function(e) {
 	e.preventDefault();
-	if(!$(this).hasClass("disabled"))
+	if (!$(this).hasClass("disabled"))
 		$("#CreateUser").modal('show');
 	else
 		$("#CreateUser").modal('hide');
 });
 
-$("a[href='#EditUser']").on("click", function(e) {
-	var userId = parseInt($(this).parents().eq(1).find("td:nth-child(1)").text());
-	var userEmail = $(this).parents().eq(1).find("td:nth-child(2)").text();
-	
-	$("#EditUser #dlgLabel").text("Edit User - "+userEmail);
-	$("#EditUser").find("input[name='txtUserEmail']").val(userEmail);
-});
+$("a[href='#EditUser']").on(
+		"click",
+		function(e) {
+			var userId = parseInt($(this).parents().eq(1).find(
+					"td:nth-child(1)").text());
+			var userEmail = $(this).parents().eq(1).find("td:nth-child(2)")
+					.text();
 
-/********************** General methods ***********************/
+			$("#EditUser #dlgLabel").text("Edit User - " + userEmail);
+			$("#EditUser").find("input[name='txtUserEmail']").val(userEmail);
+		});
+
+/** ******************** General methods ********************** */
 
 //
-function loadingOverlay(toggle)
-{
-	toggle? $("body").append(LOADER_OVERLAY) :  $("body").find(".loading-overlay").remove();
+function loadingOverlay(toggle) {
+	toggle ? $("body").append(LOADER_OVERLAY) : $("body").find(
+			".loading-overlay").remove();
 }
 
-//Model Windows Show Function 
-function showPopup(templateHTML)
-{
-	$('#popupLink').avgrund({			
-		width: 380, // max is 640px
-		height: 280, // max is 350px
-		showClose: true, // switch to 'true' for enabling close button 
-		showCloseText: 'Close', // type your text for close button
-		closeByEscape: true, // enables closing popup by 'Esc'..
-		closeByDocument: true, // ..and by clicking document itself
-		holderClass: '', // lets you name custom class for popin holder..
-		overlayClass: '', // ..and overlay block
-		enableStackAnimation: false, // another animation type
-		onBlurContainer: '', // enables blur filter for specified block 
-		template: templateHTML
+// Model Windows Show Function
+function showPopup(templateHTML) {
+	$('#popupLink').avgrund({
+		width : 380, // max is 640px
+		height : 280, // max is 350px
+		showClose : true, // switch to 'true' for enabling close button
+		showCloseText : 'Close', // type your text for close button
+		closeByEscape : true, // enables closing popup by 'Esc'..
+		closeByDocument : true, // ..and by clicking document itself
+		holderClass : '', // lets you name custom class for popin holder..
+		overlayClass : '', // ..and overlay block
+		enableStackAnimation : false, // another animation type
+		onBlurContainer : '', // enables blur filter for specified block
+		template : templateHTML
 	}).click();
 }
 
-//Get Cookie from Browser
-function getCookie( check_name ) {
-    // first we'll split this cookie up into name/value pairs
-    // note: document.cookie only returns name=value, not the other components
-    var a_all_cookies = document.cookie.split( ';' );
-    var a_temp_cookie = '';
-    var cookie_name = '';
-    var cookie_value = '';
-    var b_cookie_found = false; // set boolean t/f default f
+// Get Cookie from Browser
+function getCookie(check_name) {
+	// first we'll split this cookie up into name/value pairs
+	// note: document.cookie only returns name=value, not the other components
+	var a_all_cookies = document.cookie.split(';');
+	var a_temp_cookie = '';
+	var cookie_name = '';
+	var cookie_value = '';
+	var b_cookie_found = false; // set boolean t/f default f
 
-    for ( i = 0; i < a_all_cookies.length; i++ )
-    {
-        // now we'll split apart each name=value pair
-        a_temp_cookie = a_all_cookies[i].split( '=' );
+	for (i = 0; i < a_all_cookies.length; i++) {
+		// now we'll split apart each name=value pair
+		a_temp_cookie = a_all_cookies[i].split('=');
 
+		// and trim left/right whitespace while we're at it
+		cookie_name = a_temp_cookie[0].replace(/^\s+|\s+$/g, '');
 
-        // and trim left/right whitespace while we're at it
-        cookie_name = a_temp_cookie[0].replace(/^\s+|\s+$/g, '');
-
-        // if the extracted name matches passed check_name
-        if ( cookie_name == check_name )
-        {
-            b_cookie_found = true;
-            // we need to handle case where cookie has no value but exists (no = sign, that is):
-            if ( a_temp_cookie.length > 1 )
-            {
-                cookie_value = unescape( a_temp_cookie[1].replace(/^\s+|\s+$/g, '') );
-            }
-            // note that in cases where cookie is initialized but no value, null is returned
-            return cookie_value.replace(/"/g,"");
-            break;
-        }
-        a_temp_cookie = null;
-        cookie_name = '';
-    }
-    if ( !b_cookie_found )
-    {
-        return null;
-    }
+		// if the extracted name matches passed check_name
+		if (cookie_name == check_name) {
+			b_cookie_found = true;
+			// we need to handle case where cookie has no value but exists (no =
+			// sign, that is):
+			if (a_temp_cookie.length > 1) {
+				cookie_value = unescape(a_temp_cookie[1].replace(/^\s+|\s+$/g,
+						''));
+			}
+			// note that in cases where cookie is initialized but no value, null
+			// is returned
+			return cookie_value.replace(/"/g, "");
+			break;
+		}
+		a_temp_cookie = null;
+		cookie_name = '';
+	}
+	if (!b_cookie_found) {
+		return null;
+	}
 }
 
-
-$("#btnLogout").click(function(e){
+$("#btnLogout").click(function(e) {
 	e.preventDefault();
-	window.location="Logout";
+	window.location = "Logout";
 })
