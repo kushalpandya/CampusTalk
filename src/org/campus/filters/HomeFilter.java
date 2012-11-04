88be3f44805824e7c2684eebef8b1263b5b65511
faishal <saiyedfaishal@gmail.com>
2012-11-04 06:33:09 +0530

    Home page redirection filter if logged in added

---
+++ src/org/campus/filters/HomeFilter.java
@@ -0,0 +1,70 @@
+package org.campus.filters;
+
+import java.io.IOException;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class HomeFilter
+ */
+@WebFilter(dispatcherTypes = {
+				DispatcherType.REQUEST, 
+				DispatcherType.FORWARD, 
+				DispatcherType.INCLUDE, 
+				DispatcherType.ERROR
+		}
+					, urlPatterns = { "/index.html","/" })
+public class HomeFilter implements Filter {
+	private FilterConfig config;
+    /**
+     * Default constructor. 
+     */
+    public HomeFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		System.out.println("Home filter Test ");
+		// pass the request along the filter chain
+		HttpSession session = ((HttpServletRequest) request).getSession();
+		String logged = (String) session.getAttribute("UserEmail");
+		if (logged != null) {
+			//hres.sendRedirect(Settings.APPURL + "home.jsp");
+			RequestDispatcher rd = request.getRequestDispatcher("/home.jsp");
+			rd.forward(request, response);
+			
+		}
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		this.config = fConfig;
+	}
+
+}
