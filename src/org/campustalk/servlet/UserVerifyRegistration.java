612053a44ee101553d8c60c72392febbd6ce5620
Kushal Pandya <kushal.pandya04@gmail.com>
2012-11-07 14:58:50 +0530

    Error/Success response messages updated, code structure reformatted.

--- src/org/campustalk/servlet/UserVerifyRegistration.java
+++ src/org/campustalk/servlet/UserVerifyRegistration.java
@@ -17,74 +17,86 @@ import org.json.JSONObject;
 /**
  * Servlet implementation class UserVerifyRegistration
  */
-@WebServlet("/User/Registaration/Verify")
-public class UserVerifyRegistration extends HttpServlet {
+@WebServlet("/User/Registration/Verify")
+public class UserVerifyRegistration extends HttpServlet
+{
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UserVerifyRegistration() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public UserVerifyRegistration()
+	{
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
 		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
 		JSONObject jResponse = new JSONObject();
 		dbUser objUser = new dbUser();
-		String email,aString;
-		try {
+		String email, aString;
+		try
+		{
 			jResponse.put("status", "fail");
-			jResponse.put("message", "Something Wrong!, Please Try Again");
-		
-		String responseMsg="";
-		if(request.getParameter("e")==null || request.getParameter("q")==null){
-			responseMsg="Invalid Parameters";
-		}else {
-			email=request.getParameter("e");
-			aString=request.getParameter("q");
-			int qResp =objUser.verifyNewUser(email, aString);
-			switch(qResp){
-			case 0:
-				jResponse.put("status", "success");
-				responseMsg="Email Verified,Please Login ";
-				break;
-			case 1:
-				//User Already Verified,Please Login
-				responseMsg="Email ALready Verified,Please Login ";
-				break;
-			case 2:
-				//Invalid Url
-				responseMsg="Invalid Verification URL, Please Check";
-				break;
-				
-			case 3:
-				responseMsg="URL Expired, Please Login to get New Link";
-				break;
-			default:	
-				responseMsg="Something Wrong !";
+			jResponse.put("message",
+					"Oops! something went wrong. Please try again.");
+
+			String responseMsg = "";
+			if (request.getParameter("e") == null
+					|| request.getParameter("q") == null)
+			{
+				responseMsg = "Invalid Parameters";
 			}
-			jResponse.put("message", responseMsg);
+			else
+			{
+				email = request.getParameter("e");
+				aString = request.getParameter("q");
+				int qResp = objUser.verifyNewUser(email, aString);
+				switch (qResp)
+				{
+					case 0:
+						jResponse.put("status", "success");
+						responseMsg = "Email Verified, Please Login using your username and password.";
+						break;
+					case 1:
+						// User Already Verified,Please Login
+						responseMsg = "Email Already Verified, Please Login using your username and password.";
+						break;
+					case 2:
+						// Invalid Url
+						responseMsg = "Invalid Verification URL, Please check URL before sign-up.";
+						break;
+					case 3:
+						responseMsg = "Verification URL Expired! Please Login to get verification mail.";
+						break;
+					default:
+						responseMsg = "Oops! something went wrong. Please try again.";
+				}
+				jResponse.put("message", responseMsg);
+			}
 		}
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
+		catch (JSONException e)
+		{
 			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+		}
+		catch (ClassNotFoundException e)
+		{
 			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+		}
+		catch (SQLException e)
+		{
 			e.printStackTrace();
 		}
 		response.setContentType("application/json");
 		response.setCharacterEncoding("UTF-8");
 		out.println(jResponse);
-		
 	}
 
 }
