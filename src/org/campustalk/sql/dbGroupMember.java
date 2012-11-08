e621d5e745ae3ade99ae0e1e6d674b3b508002e7
faishal <saiyedfaishal@gmail.com>
2012-11-08 22:18:08 +0530

    get group member data function updated

--- src/org/campustalk/sql/dbGroupMember.java
+++ src/org/campustalk/sql/dbGroupMember.java
@@ -1,85 +1,112 @@
 package org.campustalk.sql;
 
-
 import java.sql.CallableStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 
-public class dbGroupMember extends DatabaseManager {
-	
-	public dbGroupMember() {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class dbGroupMember extends DatabaseManager
+{
+
+	public dbGroupMember()
+	{
 		// TODO Auto-generated constructor stub
 		super();
 	}
 
-	
-public boolean AddGroupMember(String groupname,String email,String position)throws SQLException, ClassNotFoundException {
-		
+	public boolean AddGroupMember(String groupname, String email,
+			String position) throws SQLException, ClassNotFoundException
+	{
+
 		this.open();
-		CallableStatement csSql = CON
-				.prepareCall("{call insertGroup(?,?,?)}");
+		CallableStatement csSql = CON.prepareCall("{call insertGroup(?,?,?)}");
 
 		/**
-		 * CREATE PROCEDURE `campustalk`.`insertGroupMember`( 
-		 * IN name VARCHAR(100), IN email VARCHAR(50),IN position VARCHAR(100), IN status CHAR(2))
+		 * CREATE PROCEDURE `campustalk`.`insertGroupMember`( IN name
+		 * VARCHAR(100), IN email VARCHAR(50),IN position VARCHAR(100), IN
+		 * status CHAR(2))
 		 */
-		csSql.setString(1,groupname);
-		csSql.setString(2,email);
-		csSql.setString(3,position);
+		csSql.setString(1, groupname);
+		csSql.setString(2, email);
+		csSql.setString(3, position);
 		csSql.registerOutParameter(4, Types.BOOLEAN);
-		
+
 		csSql.executeUpdate();
-		
-		boolean rtnFlag=csSql.getBoolean(3);
+
+		boolean rtnFlag = csSql.getBoolean(3);
 		csSql.close();
 		return rtnFlag;
 	}
-	
-	
-public void EditGroupMember(int groupid,int userid,String position,String status)throws SQLException, ClassNotFoundException {
-		
+
+	public void EditGroupMember(int groupid, int userid, String position,
+			String status) throws SQLException, ClassNotFoundException
+	{
+
 		this.open();
 		CallableStatement csSql = CON
 				.prepareCall("{call updateGroupMember(?,?,?,?)}");
 
-		csSql.setInt(1,groupid);
+		csSql.setInt(1, groupid);
 		csSql.setInt(2, userid);
-		csSql.setString(3,position);
-		csSql.setString(4,status);
-		
-	
+		csSql.setString(3, position);
+		csSql.setString(4, status);
+
 		csSql.executeUpdate();
 		csSql.close();
 	}
-	
-	
-public void DeleteGroupMember(int groupid,int userid) throws SQLException, ClassNotFoundException{
 
-	
-	this.open();
-	CallableStatement csSql = CON
-			.prepareCall("{call deleteGroupMembers(?,?)}");
-	csSql.setInt(1, groupid);		
-	csSql.setInt(2,userid);
-	
-	csSql.executeQuery();
-}
-
-public ResultSet getGroupMemberData(int groupid)throws SQLException, ClassNotFoundException 
-{
-	
-	this.open();
-	CallableStatement csSql = CON
-			.prepareCall("{call getGroupMemberData(?)}");
-	
-	csSql.setInt(1, groupid);
-	ResultSet rs = csSql.executeQuery();
-	while(rs.next())
+	public void DeleteGroupMember(int groupid, int userid) throws SQLException,
+			ClassNotFoundException
 	{
-		System.out.println("Hello");
+
+		this.open();
+		CallableStatement csSql = CON
+				.prepareCall("{call deleteGroupMembers(?,?)}");
+		csSql.setInt(1, groupid);
+		csSql.setInt(2, userid);
+
+		csSql.executeQuery();
 	}
-	return rs;		
-}	
-	
+
+	public JSONArray getGroupMemberData(int groupid) throws SQLException,
+			ClassNotFoundException
+	{
+		JSONArray groupmember_arr = new JSONArray();
+		try
+		{
+			this.open();
+			CallableStatement csSql = CON
+					.prepareCall("{call getGroupMemberData(?)}");
+
+			csSql.setInt(1, groupid);
+			ResultSet rs = csSql.executeQuery();
+
+			JSONObject temp;
+			System.out.println("error before while ++++++");
+
+			while (rs.next())
+			{
+
+				System.out.println("in servletncjxjhvjv");
+				temp = new JSONObject();
+
+				temp.put("userid", rs.getInt("userid"));
+				temp.put("email", rs.getString("email"));
+				temp.put("name", rs.getString("name"));
+				temp.put("role", rs.getString("rname"));
+
+				System.out.print("gmember data=" + rs.getString("rname"));
+				groupmember_arr.put(temp);
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return groupmember_arr;
+	}
+
 }
