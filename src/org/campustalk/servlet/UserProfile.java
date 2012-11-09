262f7df5c4b653017bd9cfeec23e49779ee30e00
faishal <saiyedfaishal@gmail.com>
2012-11-09 08:14:44 +0530

    User profile servlet added to fetch user data

---
+++ src/org/campustalk/servlet/UserProfile.java
@@ -0,0 +1,71 @@
+package org.campustalk.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.campustalk.sql.dbUser;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Servlet implementation class UserProfile
+ */
+@WebServlet("/User/Profile")
+public class UserProfile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserProfile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession(true);
+		PrintWriter out = response.getWriter();
+		JSONObject jResponse = new JSONObject();
+				
+		String status="fail";
+		String errormsg= "Something Wrong !!";
+		
+		if(session.getAttribute("UserId")==null){
+			errormsg= "Session expired, Please Reload Page";
+		}
+		else{
+			String type= request.getParameter("type");
+			dbUser objUser= new dbUser();
+			if(type.equalsIgnoreCase("email")){
+				jResponse = objUser.UserProfileByEmail(request.getParameter("data"));
+			}else{
+				jResponse = objUser.UserProfileById(Integer.parseInt(request.getParameter("data")));
+			}
+			status="success";
+			
+		}
+		try {
+			jResponse.put("status", status);
+			jResponse.put("message", errormsg);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		out.println(jResponse);
+	}
+
+}
